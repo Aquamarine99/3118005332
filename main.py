b8171d945828782b554bf9588855077a18f27961
Aquamarine99 <48514346+Aquamarine99@users.noreply.github.com>
2020-09-24 19:06:05 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-
 import sys
-
-from click._compat import raw_input
+from click._compat import raw_input #接收行输入
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
+import re
 import jieba
+
+print("请输入源论文地址：")
 f=open(raw_input(),"r",encoding="UTF-8")
-s1=f.read()#输入s1
+f1=f.read()
+print("请输入抄袭版论文地址：")
 f=open(raw_input(),"r",encoding="UTF-8")
-s2=f.read()#输入s2
+f2=f.read()
 f.close()
-sign='· ~ ！ @ # ￥ % …… & * （ ） —— - + = ] } [ { \ | ; :  " " < > , . /  ？ $ ^ _'
-for word in sign:
-    s1=s1.replace(word,'') #循环文本，删去标点符号
-def jaccard_similarity(s1, s2):#jaccard
-    def add_space(s):
-        s_list=jieba.cut(s,cut_all=False)
-        return ' '.join(list(s_list))# 将字中间加入空格
-    s1, s2=add_space(s1), add_space(s2)# 转化为TF矩阵
+
+def filter_words(file):
+    pt = re.compile(u"[a-zA-Z0-0\u4e00-\u9fa5]")
+    str = " "
+    str = pt.sub("",str)
+    str = jieba.cut(file,cut_all=False)
+    return ' '.join(list(str)) #转化为TF矩阵
+
+str1 = filter_words(f1)
+str2 = filter_words(f2)
+
+def jaccard_similarity(str1, str2):
     cv=CountVectorizer(tokenizer=lambda s: s.split())
-    corpus=[s1, s2]
+    corpus=[str1, str2]
     vectors=cv.fit_transform(corpus).toarray()# 求交集
     numerator=np.sum(np.min(vectors, axis=0))# 求并集
     denominator=np.sum(np.max(vectors, axis=0))# 计算杰卡德系数
     return 1.0 * numerator / denominator
 
-#print("orig_0.8_dis_1的测试结果：{:f}".format(jaccard_similarity(s1, s2)))#输出查重结果
+print("请输入输出答案地址：")
 f=open(raw_input(),"w",encoding="UTF-8")
-f.write( str(jaccard_similarity(s1, s2))[0:4] )
+f.write( str(jaccard_similarity(str1, str2))[0:4] ) #输出查重结果
 f.close()
